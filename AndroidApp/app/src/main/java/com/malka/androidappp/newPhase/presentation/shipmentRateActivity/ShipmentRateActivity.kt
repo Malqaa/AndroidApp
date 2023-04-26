2413bbfc7722850cccac6cc5fc94a089d97c12fa
DESKTOP-IHFUQ37\user <maged.eldamasy@gmail.com>
2023-04-26 21:11:29 +0200

    no message

---
+++ AndroidApp/app/src/main/java/com/malka/androidappp/newPhase/presentation/shipmentRateActivity/ShipmentRateActivity.kt
@@ -0,0 +1,11 @@
+package com.malka.androidappp.newPhase.presentation.shipmentRateActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShipmentRateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shipment_rate)
+    }
+}
