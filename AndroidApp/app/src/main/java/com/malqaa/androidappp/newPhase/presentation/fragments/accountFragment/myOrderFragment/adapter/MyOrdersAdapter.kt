a76b58fe7f52dc3e4c3a9dec2fa449d36c20ef93
abdelazizdaoud <abdelaz9z@icloud.com>
2024-08-29 20:42:20 +0300

    Fix show order number in (my order - received)

--- AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/fragments/accountFragment/myOrderFragment/adapter/MyOrdersAdapter.kt
+++ AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/fragments/accountFragment/myOrderFragment/adapter/MyOrdersAdapter.kt
@@ -1,24 +1,26 @@
 package com.malqaa.androidappp.newPhase.presentation.fragments.accountFragment.myOrderFragment.adapter
 
+
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.malqaa.androidappp.R
-
-
 import com.malqaa.androidappp.databinding.ItemUserOrderBinding
+import com.malqaa.androidappp.newPhase.domain.models.orderListResp.OrderItem
+import com.malqaa.androidappp.newPhase.utils.ConstantObjects
 import com.malqaa.androidappp.newPhase.utils.HelpFunctions
 import com.malqaa.androidappp.newPhase.utils.hide
 import com.malqaa.androidappp.newPhase.utils.show
-import com.malqaa.androidappp.newPhase.domain.models.orderListResp.OrderItem
-import com.malqaa.androidappp.newPhase.utils.ConstantObjects
-import kotlinx.android.synthetic.main.activity_order_details.tv_request_type
 
-class MyOrdersAdapter(var orderList: List<OrderItem>, var setOnClickListeners:SetOnClickListeners) :
+class MyOrdersAdapter(
+    private var orderList: List<OrderItem>,
+    private var setOnClickListeners: SetOnClickListeners
+) :
     Adapter<MyOrdersAdapter.SoldOutOrdersViewHolder>() {
-    var currentOrder=true
+    var currentOrder = true
+
     class SoldOutOrdersViewHolder(var viewBinding: ItemUserOrderBinding) :
         RecyclerView.ViewHolder(viewBinding.root)
 
@@ -34,25 +36,23 @@ class MyOrdersAdapter(var orderList: List<OrderItem>, var setOnClickListeners:Se
     override fun getItemCount(): Int = orderList.size
 
     override fun onBindViewHolder(holder: SoldOutOrdersViewHolder, position: Int) {
-        if(currentOrder) {
-            holder.viewBinding.orderNumberTv.text = "#${orderList[position].orderMasterId}"
-        }else{
-            holder.viewBinding.orderNumberTv.text = "#${orderList[position].orderId}"
+        holder.viewBinding.orderNumberTv.text = "#${orderList[position].orderMasterId}"
+
+        if (orderList[position].requestType?.lowercase().equals("FixedPrice".lowercase())) {
+            holder.viewBinding.tvRequestType.text = context.getString(R.string.fixed_price)
+        } else if (orderList[position].requestType?.lowercase().equals("Negotiation".lowercase())) {
+            holder.viewBinding.tvRequestType.text = context.getString(R.string.Negotiation)
+        } else if (orderList[position].requestType?.lowercase().equals("Auction".lowercase())) {
+            holder.viewBinding.tvRequestType.text = context.getString(R.string.auction)
         }
 
-
-
-        if(orderList[position].requestType?.lowercase().equals("FixedPrice".lowercase())){
-            holder.viewBinding.tvRequestType.text=context.getString(R.string.fixed_price)
-        }else if(orderList[position].requestType?.lowercase().equals("Negotiation".lowercase())){
-            holder.viewBinding.tvRequestType.text=context.getString(R.string.Negotiation)
-        }else if(orderList[position].requestType?.lowercase().equals("Auction".lowercase())){
-            holder.viewBinding.tvRequestType.text=context.getString(R.string.auction)
-        }
-
-        holder.viewBinding.orderTimeTv.text = HelpFunctions.getViewFormatForDateTrackWithoutUTC(orderList[position].createdAt,"dd/MM/yyyy HH:mm:ss");
-        holder.viewBinding.shipmentsTv.text=orderList[position].providersCount.toString()
-        holder.viewBinding.totalOrderTv.text = "${orderList[position].totalOrderAmountAfterDiscount} ${context.getString(R.string.rial)}"
+        holder.viewBinding.orderTimeTv.text = HelpFunctions.getViewFormatForDateTrackWithoutUTC(
+            orderList[position].createdAt,
+            "dd/MM/yyyy HH:mm:ss"
+        );
+        holder.viewBinding.shipmentsTv.text = orderList[position].providersCount.toString()
+        holder.viewBinding.totalOrderTv.text =
+            "${orderList[position].totalOrderAmountAfterDiscount} ${context.getString(R.string.rial)}"
 
 //        when ( orderList[position].orderStatus) {
 //            ConstantObjects.WaitingForPayment -> {
@@ -71,25 +71,25 @@ class MyOrdersAdapter(var orderList: List<OrderItem>, var setOnClickListeners:Se
 //                holder.viewBinding.orderStatusTv.text = context.getString(R.string.canceled)
 //            }
 //            else ->{
-                holder.viewBinding.orderStatusTv.text = orderList[position].status ?: ""
+        holder.viewBinding.orderStatusTv.text = orderList[position].status ?: ""
 //            }
 //        }
 
-        if(currentOrder){
+        if (currentOrder) {
             holder.viewBinding.completeOrderBtn.hide()
-        }else{
+        } else {
             holder.viewBinding.completeOrderBtn.show()
         }
-     holder.viewBinding.itemView.setOnClickListener {
-         setOnClickListeners.onOrderSelected(position)
-     }
+        holder.viewBinding.itemView.setOnClickListener {
+            setOnClickListeners.onOrderSelected(position)
+        }
         holder.viewBinding.completeOrderBtn.setOnClickListener {
             setOnClickListeners.onOrderSelected(position)
         }
-   ConstantObjects
+        ConstantObjects
     }
 
-    interface SetOnClickListeners{
+    interface SetOnClickListeners {
         fun onOrderSelected(position: Int)
     }
 
