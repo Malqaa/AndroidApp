20f52a4b1966058f4ed8cba031962d6aa692e811
abdelazizdaoud <abdelaz9z@icloud.com>
2024-09-03 18:52:55 +0300

    fix unable to destroy activity

--- AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/productDetailsActivity/MyProductDetailsActivity.kt
+++ AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/productDetailsActivity/MyProductDetailsActivity.kt
@@ -1176,8 +1176,8 @@ class MyProductDetailsActivity : BaseActivity(), SwipeRefreshLayout.OnRefreshLis
         super.onDestroy()
         productDetialsViewModel.closeAllCall()
         productDetialsViewModel.baseCancel()
-        similarProductAdapter.onDestroyHandler()
-        sellerProductAdapter.onDestroyHandler()
+//        similarProductAdapter.onDestroyHandler()
+//        sellerProductAdapter.onDestroyHandler()
     }
 
     override fun onClickImage(url: String) {
