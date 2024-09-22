37e32ad49df3f1e19f20155e0818770eb2fa6b32
abdelazizdaoud <abdelaz9z@icloud.com>
2024-09-22 14:06:41 +0300

    Fix NullPointerException in DynamicTemplateActivity by ensuring non-null values for text fields

--- AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/addProduct/activity5/DynamicTemplateActivtiy.kt
+++ AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/addProduct/activity5/DynamicTemplateActivtiy.kt
@@ -1,9 +1,7 @@
 package com.malqaa.androidappp.newPhase.presentation.activities.addProduct.activity5
 
-import android.R.attr.duration
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
@@ -20,8 +18,11 @@ import com.malqaa.androidappp.newPhase.utils.HelpFunctions
 import com.malqaa.androidappp.newPhase.utils.hide
 import com.malqaa.androidappp.newPhase.utils.linearLayoutManager
 import com.malqaa.androidappp.newPhase.utils.show
-import kotlinx.android.synthetic.main.fragment_dynamic_template.*
-import kotlinx.android.synthetic.main.toolbar_main.*
+import kotlinx.android.synthetic.main.fragment_dynamic_template.btn_dynamic_next
+import kotlinx.android.synthetic.main.fragment_dynamic_template.progressBar
+import kotlinx.android.synthetic.main.fragment_dynamic_template.rvDynamicList
+import kotlinx.android.synthetic.main.toolbar_main.back_btn
+import kotlinx.android.synthetic.main.toolbar_main.toolbar_title
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -29,19 +30,6 @@ import kotlinx.coroutines.withContext
 
 class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnChangeValueListener {
 
-    //    var file_name: String = ""
-//    var Title: String = ""
-//
-//    val json_key_data: String = "data";
-//    val json_key_id: String = "id";
-//    val json_key_title: String = "title";
-//    val json_key_placeholder: String = "placeholder";
-//    val type: String = "type";
-//    val json_key_enum: String = "enum";
-//    var fm: FragmentManager? = null
-//
-//    var selectdate = ""
-//    var selectTime = ""
     private var dataList: ArrayList<DynamicSpecificationSentObject>? = null
     private var isEdit: Boolean = false
     private lateinit var addProductViewModel: AddProductViewModel
@@ -59,9 +47,7 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
         dataList = arrayListOf()
         isEdit = intent.getBooleanExtra(ConstantObjects.isEditKey, false)
 
-        //  println("hhh  cat id : ${AddProductObjectData.selectedCategoryId}")
         addProductViewModel.getDynamicSpecification(AddProductObjectData.selectedCategoryId)
-        // addProductViewModel.getDynamicSpecification(50)
     }
 
     private fun setDataForUpdate() {
@@ -69,7 +55,8 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
             selectSpecificationsArrayList = dynamicSpecificationsArrayList
             for (item in selectSpecificationsArrayList) {
                 if (item.type == 2 || item.type == 3 || item.type == 4) {
-                    val obj = AddProductObjectData.productSpecificationList!!.find { it.SpecificationId == item.id }
+                    val obj =
+                        AddProductObjectData.productSpecificationList!!.find { it.SpecificationId == item.id }
                     if (obj != null) {
                         item.setData(obj)
                         item.subSpecifications?.add(
@@ -77,23 +64,20 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                                 id = obj.SpecificationId,
                                 nameAr = obj.ValueSpeAr,
                                 nameEn = obj.ValueSpeEn,
-                                isDataSelected=true
+                                isDataSelected = true
                             )
                         )
                     }
-                }
-                else if(item.type == 5 || item.type == 6){
+                } else if (item.type == 5 || item.type == 6) {
 
-                }
-
-                else {
+                } else {
                     item.subSpecifications?.forEach {
-                        for(j in AddProductObjectData.productSpecificationList!!){
-                            if(j.ValueSpeAr == it.id.toString()){
+                        for (j in AddProductObjectData.productSpecificationList!!) {
+                            if (j.ValueSpeAr == it.id.toString()) {
                                 item.setData(j)
-                                it.isDataSelected=true
+                                it.isDataSelected = true
                             }
-                    }
+                        }
                     }
                 }
 
@@ -181,18 +165,9 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
             finish()
         }
 
-        btn_dynamic_next.setOnClickListener() {
+        btn_dynamic_next.setOnClickListener {
             checkAllDataSet(dataList!!)
-            // GetDataFromDynamicControl()
-
         }
-        //   btnSubCatgeoryFilter.setOnClickListener {
-//            startActivity(Intent(this, SubCategories::class.java).apply {
-//                putExtra("categoryid", allCategoryList[position].categoryKey.toString())
-//                putExtra("categoryName", allCategoryList[position].categoryName.toString())
-//            })
-        //  }
-
     }
 
     private fun checkAllDataSet(data: ArrayList<DynamicSpecificationSentObject>) {
@@ -201,19 +176,21 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
 
             dynamicSpecificationsArrayList.forEach { dynamicSpecificationItem ->
 
-                val supIdAr = dynamicSpecificationItem.subSpecifications?.find { it.nameAr == dynamicSpecificationItem.valueArText }
-                val supIdEn = dynamicSpecificationItem.subSpecifications?.find { it.nameEn == dynamicSpecificationItem.valueEnText }
+                val supIdAr =
+                    dynamicSpecificationItem.subSpecifications?.find { it.nameAr == dynamicSpecificationItem.valueArText }
+                val supIdEn =
+                    dynamicSpecificationItem.subSpecifications?.find { it.nameEn == dynamicSpecificationItem.valueEnText }
 
                 if (dynamicSpecificationItem.type == 1) {
-                    if (dynamicSpecificationItem.subSpecifications != null && dynamicSpecificationItem.subSpecifications!!.isNotEmpty()) {
-                        if (dynamicSpecificationItem.valueArText.isEmpty()) {
+                    if (!dynamicSpecificationItem.subSpecifications.isNullOrEmpty()) {
+                        if (dynamicSpecificationItem.valueArText.isNullOrEmpty()) {
                             allValuesSet = false
                         } else {
                             val newObject = DynamicSpecificationSentObject(
-                                HeaderSpeAr = dynamicSpecificationItem.nameAr.toString(),
-                                HeaderSpeEn = dynamicSpecificationItem.nameEn.toString(),
-                                ValueSpeAr = supIdAr?.id.toString(),
-                                ValueSpeEn = supIdEn?.id.toString(),
+                                HeaderSpeAr = dynamicSpecificationItem.nameAr ?: "",
+                                HeaderSpeEn = dynamicSpecificationItem.nameEn ?: "",
+                                ValueSpeAr = supIdAr?.id?.toString() ?: "",
+                                ValueSpeEn = supIdEn?.id?.toString() ?: "",
                                 Type = dynamicSpecificationItem.type,
                                 SpecificationId = dynamicSpecificationItem.id
                             )
@@ -226,7 +203,8 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                 } else if (dynamicSpecificationItem.type == 5 || dynamicSpecificationItem.type == 6) {
                     val required = dynamicSpecificationItem.isRequired
                     if (required) {
-                        val supIdAr = dynamicSpecificationItem.subSpecifications?.find { dynamicSpecificationItem.valueBoolean }
+                        val supIdAr =
+                            dynamicSpecificationItem.subSpecifications?.find { dynamicSpecificationItem.valueBoolean }
                         if (supIdAr == null) {
                             allValuesSet = false
                             if (dynamicSpecificationItem.isRequired) {
@@ -239,10 +217,10 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                             }
                         } else {
                             val newObject = DynamicSpecificationSentObject(
-                                HeaderSpeAr = dynamicSpecificationItem.nameAr.toString(),
-                                HeaderSpeEn = dynamicSpecificationItem.nameEn.toString(),
+                                HeaderSpeAr = dynamicSpecificationItem.nameAr ?: "",
+                                HeaderSpeEn = dynamicSpecificationItem.nameEn ?: "",
                                 ValueSpeAr = supIdAr.id.toString(),
-                                ValueSpeEn = supIdEn?.id.toString(),
+                                ValueSpeEn = supIdEn?.id?.toString() ?: "",
                                 Type = dynamicSpecificationItem.type,
                                 SpecificationId = dynamicSpecificationItem.id
                             )
@@ -253,15 +231,15 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                         }
                     }
                 } else if (dynamicSpecificationItem.type == 4) {
-                    if (dynamicSpecificationItem.isRequired && dynamicSpecificationItem.valueArText.isEmpty()) {
+                    if (dynamicSpecificationItem.isRequired && dynamicSpecificationItem.valueArText.isNullOrEmpty()) {
                         allValuesSet = false
                     } else {
                         val newObject = DynamicSpecificationSentObject(
-                            HeaderSpeAr = dynamicSpecificationItem.nameAr.toString(),
-                            HeaderSpeEn = dynamicSpecificationItem.nameAr.toString(),
+                            HeaderSpeAr = dynamicSpecificationItem.nameAr ?: "",
+                            HeaderSpeEn = dynamicSpecificationItem.nameAr ?: "",
                             ValueSpeAr = dynamicSpecificationItem.valueArText ?: "",
                             Type = dynamicSpecificationItem.type,
-                            ValueSpeEn = dynamicSpecificationItem.valueArText?: "",
+                            ValueSpeEn = dynamicSpecificationItem.valueArText ?: "",
                             SpecificationId = dynamicSpecificationItem.id
                         )
 
@@ -270,15 +248,15 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                         }
                     }
                 } else {
-                    if (dynamicSpecificationItem.isRequired && dynamicSpecificationItem.valueArText.isEmpty()) {
+                    if (dynamicSpecificationItem.isRequired && dynamicSpecificationItem.valueArText.isNullOrEmpty()) {
                         allValuesSet = false
                     } else {
                         val newObject = DynamicSpecificationSentObject(
-                            HeaderSpeAr = dynamicSpecificationItem.nameAr.toString(),
-                            HeaderSpeEn = dynamicSpecificationItem.nameEn.toString(),
-                            ValueSpeAr = dynamicSpecificationItem.valueArText?: "",
+                            HeaderSpeAr = dynamicSpecificationItem.nameAr ?: "",
+                            HeaderSpeEn = dynamicSpecificationItem.nameEn ?: "",
+                            ValueSpeAr = dynamicSpecificationItem.valueArText ?: "",
                             Type = dynamicSpecificationItem.type,
-                            ValueSpeEn = dynamicSpecificationItem.valueEnText?: "",
+                            ValueSpeEn = dynamicSpecificationItem.valueEnText ?: "",
                             SpecificationId = dynamicSpecificationItem.id
                         )
 
@@ -348,169 +326,5 @@ class DynamicTemplateActivtiy : BaseActivity(), DynamicSpecificationsAdapter.OnC
                 spinnerPosition
             )?.nameEn ?: ""
     }
-
-
-//    fun CreateControlsFromJson() {
-//        try {
-//            if (file_name.trim().length > 0) {
-//                HelpFunctions.GetTemplatesJson(
-//                    file_name
-//                ) { json_string ->
-//                    if (json_string.trim().length > 0) {
-//                        val parsed_data = JSONObject(json_string)
-//                        val controls_array: JSONArray = parsed_data.getJSONArray(json_key_data);
-//                        if (controls_array.length() > 0) {
-//                            for (i in 0 until controls_array.length()) {
-//                                val IndControl = controls_array.getJSONObject(i)
-//                                if (IndControl != null) {
-//                                    AddControlToScreen(IndControl);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//            }
-//        } catch (ex: Exception) {
-//            HelpFunctions.ReportError(ex);
-//        }
-//    }
-//
-//    fun AddControlToScreen(IndControl: JSONObject) {
-//        try {
-//            val isdropdown: Boolean = IndControl.has(json_key_enum);
-//
-//            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//            val _view: View;
-//            if (!isdropdown) {
-//                _view = inflater.inflate(R.layout.dynamic_text_field, null)
-//
-//                val txt_dynamic: TextView = _view.findViewById(R.id.dynamic_text_field)
-//                txt_dynamic.text =
-//                    if (IndControl.has(json_key_placeholder)) IndControl.getString(
-//                        json_key_placeholder
-//                    ) else ""
-//
-//                val txt_dynamic_text_field: EditText =
-//                    _view.findViewById(R.id.txt_dynamic_text_field)
-//
-//
-//                val iv_end_icon: ImageView =
-//                    _view.findViewById(R.id.iv_end_icon)
-//
-//                val type = IndControl.getString(
-//                    type
-//                )
-//                when (type) {
-//                    "string" -> {
-//                        txt_dynamic_text_field.inputType = InputType.TYPE_CLASS_TEXT
-//                    }
-//                    "number" -> {
-//                        txt_dynamic_text_field.inputType = InputType.TYPE_CLASS_NUMBER
-//                    }
-//                    "decimal" -> {
-//                        txt_dynamic_text_field.inputType = InputType.TYPE_NUMBER_FLAG_DECIMAL
-//                    }
-//                    "datetime" -> {
-//                        txt_dynamic_text_field.apply {
-//                            isFocusable = false
-//                            isFocusableInTouchMode = false
-//                            isClickable = true
-////                            val img = ContextCompat.getDrawable(
-////                                this@DynamicTemplate,
-////                                R.drawable.right_arrow
-////                            )
-////                            //  txt_dynamic_text_field.setCompoundDrawables(null, null, img, null)
-////                            txt_dynamic_text_field.setCompoundDrawablesWithIntrinsicBounds(
-////                                0,
-////                                0,
-////                                R.drawable.right_arrow,
-////                                0
-////                            );
-//
-//                            iv_end_icon.show()
-//                            setOnClickListener {
-//                                fm = supportFragmentManager
-//                                val dateDialog = DatePickerFragment(false, false) { selectdate ->
-//                                    val timeDialog = TimePickerFragment { selectTime ->
-//                                        txt_dynamic_text_field.setText("$selectdate - $selectTime")
-//                                    }
-//                                    timeDialog.show(fm!!, "fragment_time")
-//                                }
-//                                dateDialog.show(fm!!, "fragment_date")
-//                            }
-//                        }
-//                    }
-//                    else -> {
-//                        txt_dynamic_text_field.inputType = InputType.TYPE_CLASS_TEXT
-//
-//                    }
-//                }
-//                txt_dynamic_text_field.tag =
-//                    if (IndControl.has(json_key_id)) IndControl.getString(json_key_id) else ""
-//
-//            } else {
-//                _view = inflater.inflate(R.layout.dynamic_spinner, null)
-//                val lbl_dynamic: TextView =
-//                    _view.findViewById(R.id.lbl_dynamic_spinner);
-//                lbl_dynamic.text =
-//                    if (IndControl.has(json_key_title)) IndControl.getString(json_key_title) else ""
-//                val spin_dynamic: SpinnerComponent = _view.findViewById(R.id.spinner_dynamic);
-//                val _json_array_values = IndControl.getJSONArray(json_key_enum);
-//                if (_json_array_values.length() > 0) {
-//                    val spinner_values = Array(_json_array_values.length()) {
-//                        _json_array_values.getString(it)
-//                    }
-//
-//                    spin_dynamic.tag =
-//                        if (IndControl.has(json_key_id)) IndControl.getString(json_key_id) else ""
-//                    spin_dynamic._setOnClickListener {
-//                        val items: ArrayList<SearchListItem> = ArrayList()
-//                        spinner_values.forEachIndexed { index, item ->
-//                            items.add(SearchListItem(index, item))
-//                        }
-//                        spin_dynamic.showSpinner(this, items, "Select ${lbl_dynamic.text}") {
-//                            spin_dynamic.text = it.title
-//                        }
-//
-//                    }
-//
-//                }
-//            }
-//            parent_layout.addView(_view)
-//        } catch (ex: Exception) {
-//            HelpFunctions.ReportError(ex);
-//        }
-//    }
-//
-//    fun GetDataFromDynamicControl() {
-//        try {
-//            var num_child_views: Int = parent_layout.childCount;
-//            if (num_child_views > 0)
-//                num_child_views = num_child_views - 1
-//            for (i in 0..num_child_views) {
-//                val indcontol: View = parent_layout.getChildAt(i);
-//                if (indcontol.id == R.id.card_dynamic_text) {
-//                    val txtfield: EditText =
-//                        indcontol.findViewById(R.id.txt_dynamic_text_field)
-//                    if (txtfield.tag != null && txtfield.tag.toString().length > 0) {
-//                        ConstantObjects.dynamic_json_dictionary[txtfield.tag.toString()] =
-//                            txtfield.text.toString()
-//                    }
-//                }
-//                //Spinner
-//                else if (indcontol.id == R.id.card_dynamic_spinner) {
-//                    val _spinner: SpinnerComponent = indcontol.findViewById(R.id.spinner_dynamic)
-//                    if (_spinner.tag != null && _spinner.tag.toString().length > 0) {
-//                        ConstantObjects.dynamic_json_dictionary[_spinner.tag.toString()] =
-//                            _spinner.getText()
-//                    }
-//                }
-//            }
-//        } catch (ex: Exception) {
-//            HelpFunctions.ReportError(ex);
-//        }
-//    }
-
 
 }
