2e9c3b457dd2da9e43c7a82a36e81225b1f886a2
Abdelaziz Dawoud <abdelaz9z@icloud.com>
2025-05-02 21:20:32 +0300

    remove import for Log

--- AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/addProduct/activity7/ShippingAdapter.kt
+++ AndroidApp/app/src/main/java/com/malqaa/androidappp/newPhase/presentation/activities/addProduct/activity7/ShippingAdapter.kt
@@ -1,7 +1,6 @@
 package com.malqaa.androidappp.newPhase.presentation.activities.addProduct.activity7
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
